144f0b50ca7865ded06d39f88f731a25de745ce1
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-25 18:09:37 +0330

    Declare Error State

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -3,13 +3,21 @@ import { useState } from "react";
 
 export default function Weather() {
   const[city,setCity]= useState('');
+  const[Error,setError]=useState('');
   
+  const getData = async ()=>{
+    if(!city){
+      setError('Please Enter a city name');
+      return;
+    }
+    
+  }
 
   return (
     <div>
       <h1>Weather</h1>
       <input type="text" onChange={(e)=>setCity(e.target.value)}  value={city} />
-      <button onClick={""}>Get Data</button>
+      <button onClick={getData}>Get Data</button>
     </div>
   );
 }
