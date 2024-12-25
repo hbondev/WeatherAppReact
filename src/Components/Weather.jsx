459885c549cf37197bae5cc81c49f3be8666e4b7
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-25 18:24:43 +0330

    Displaying Data in dom

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Weather() {
   const[city,setCity]= useState('');
   const[Error,setError]=useState('');
+  const[Weather,setWeather]=useState([]);
   
   const getData = async ()=>{
     if(!city){
@@ -13,18 +14,30 @@ export default function Weather() {
     try{
       const apiKey = 'c046612a0ea6bccddab3e81e2ebaf2a9';
       const Res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-    }
-    catch{
+      setWeather([Res.data]);
+      setError('');
 
+    }
+    catch(err){
+      setError['city not found !'];
+      setWeather[''];
     }
     
   }
 
   return (
-    <div>
+    <>
       <h1>Weather</h1>
       <input type="text" onChange={(e)=>setCity(e.target.value)}  value={city} />
       <button onClick={getData}>Get Data</button>
-    </div>
+    
+      <ul>
+        {Weather.map((data)=>{
+          return <li key={data.id}>{data.name} , {data.main.temp}°C , {data.weather[0].description}</li>
+        })}
+      </ul>
+
+      {/* { JSX } */}
+    </>
   );
 }
