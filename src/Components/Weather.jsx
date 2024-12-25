0f4d07005d453458b5756b3397eda91cebd495f8
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-25 17:54:42 +0330

    Create Weather Component

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,23 +2,13 @@ import axios from "axios";
 import { useEffect } from "react";
 
 export default function Weather() {
-    useEffect(() => {
-        axios({
-            method: 'get',
-            url: 'https://jsonplaceholder.typicode.com/posts?id=1',
-          })
-            .then(function (response) {
-              console.log(response)
-            });
-    }, []);
-    function inputHandler(e) {
-      console.log(e.target.value);
-    }
+  
 
   return (
     <div>
       <h1>Weather</h1>
       <input type="text" onChange={(e)=>inputHandler(e)}  />
+      <button>Get Data</button>
     </div>
   );
 }
