ce279eed41c8ff1c6cd95198410aecfe0df4c93c
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-25 18:00:43 +0330

    Declare State for input

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,14 +1,15 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Weather() {
+  const[city,setCity]= useState('');
   
 
   return (
     <div>
       <h1>Weather</h1>
-      <input type="text" onChange={(e)=>inputHandler(e)}  />
-      <button>Get Data</button>
+      <input type="text" onChange={(e)=>setCity(e.target.value)}  value={city} />
+      <button onClick={""}>Get Data</button>
     </div>
   );
 }
